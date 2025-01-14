82dfdb5e7a956764bbce48151369cb08f2ea32e9
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-14 11:17:30 +0000

    Add games, platform models

--- profiles/models.py
+++ profiles/models.py
@@ -11,9 +11,10 @@ class Favourites(models.Model):
     game = models.CharField(max_length=50)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.game
+
 
 class Library(models.Model):
     user = models.ForeignKey(
@@ -23,14 +24,30 @@ class Library(models.Model):
     game = models.CharField(max_length=50)
     platform = models.CharField(max_length=50)
     completed = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.user.username
+
 
 class Privacy(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='privacy')
     public = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.user.username
+
+
+class Games(models.Model):
+    game = models.CharField(max_length=50)
+    platform = models.ManyToManyField('Platforms')
+
+    def __str__(self):
+        return self.game
+
+
+class Platforms(models.Model):
+    platform = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.platform
