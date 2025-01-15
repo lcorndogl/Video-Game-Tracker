85327a1b81726b8481c87d1f52a041a493a1c00a
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-15 10:54:37 +0000

    Collect static files

--- profiles/models.py
+++ profiles/models.py
@@ -7,17 +7,15 @@ from django.contrib.auth.models import User
 class Favourite(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='favourite')
-    platform = models.CharField(max_length=50)
-    game = models.CharField(max_length=50)
-    game2 = models.ForeignKey(
+    game = models.ForeignKey(
         'Game', on_delete=models.PROTECT, blank=True, null=True)
-    platform2 = models.ForeignKey(
+    platform = models.ForeignKey(
         'Platform', on_delete=models.PROTECT, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.game
+        return self.game.game
 
 
 class Library(models.Model):
@@ -25,9 +23,8 @@ class Library(models.Model):
         User, on_delete=models.CASCADE, related_name='library')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    game2 = models.ManyToManyField('Game')
-    game = models.CharField(max_length=50)
-    platform = models.CharField(max_length=50)
+    game = models.ManyToManyField('Game')
+    platform = models.ManyToManyField('Platform')
     completed = models.BooleanField(default=False)
 
     def __str__(self):
