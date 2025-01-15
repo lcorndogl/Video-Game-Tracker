7a2f359b087948b8bb3bb287e93374ffc589805d
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-15 10:05:24 +0000

    Admin Panel changes

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Favourite
+from .models import Favourite, Game, Platform
 from django_summernote.admin import SummernoteModelAdmin
+
 
 @admin.register(Favourite)
 class ProfileAdmin(SummernoteModelAdmin):
     list_display = ('user', 'platform', 'game', 'created_on', 'updated_on')
+
+
+@admin.register(Game)
+class GameAdmin(SummernoteModelAdmin):
+    list_display = ('game',)
+
+
+@admin.register(Platform)
+class PlatformAdmin(SummernoteModelAdmin):
+    list_display = ('platform',)
 
 # Register your models here.
