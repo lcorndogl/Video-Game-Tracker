3f2af4cc6b082d25e175bc18e36e6315318c9419
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-21 11:01:07 +0000

    Adjust detailed profile url to not be a root url

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('profiles/', views.ProfileList.as_view(), name='profiles'),
     path('profiles/manage/', views.manage_profile, name='manage'),
-    path('<str:username>/', views.profile_detailed, name='profile_detailed'),
+    path('profiles/<str:username>/', views.profile_detailed, name='profile_detailed'),
     # path('profile_view/', views.ProfileDetail.as_view(), name='profile_detailed'),
     
 ]
