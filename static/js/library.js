ac628e44dd64ab030a8c423c1a80793c9b48b736
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-26 15:48:39 +0000

    Add game name to the game removal confirmation modal

--- static/js/library.js
+++ static/js/library.js
@@ -1,5 +1,6 @@
-function confirmRemove(gameId) {
+function confirmRemove(gameId, gameName) {
     document.getElementById('remove_game_id').value = gameId;
+    document.getElementById('gameName').textContent = gameName;
     document.getElementById('confirmRemoveModal').style.display = 'block';
 }
 
@@ -7,9 +8,9 @@ function closeModal() {
     document.getElementById('confirmRemoveModal').style.display = 'none';
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     var closeButtons = document.querySelectorAll('.close, .btn-secondary');
-    closeButtons.forEach(function (button) {
+    closeButtons.forEach(function(button) {
         button.addEventListener('click', closeModal);
     });
 });
