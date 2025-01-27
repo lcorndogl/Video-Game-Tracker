e1c715f011435449d07df60727a76a03aa65ac88
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-27 13:57:43 +0000

    order profiles by updated date

--- profiles/views.py
+++ profiles/views.py
@@ -109,9 +109,9 @@ def home(request):
 class ProfileList(generic.ListView):
     def get_queryset(self):
         if self.request.user.is_authenticated:
-            return User_Profile.objects.filter(privacy__in=["1", "3"]).order_by("-created_on")
+            return User_Profile.objects.filter(privacy__in=["1", "3"]).order_by("-updated_on")
         else:
-            return User_Profile.objects.filter(privacy__lt="2").order_by("-created_on")
+            return User_Profile.objects.filter(privacy__lt="2").order_by("-updated_on")
 
     context_object_name = 'profiles'
     template_name = "profiles/profiles.html"
